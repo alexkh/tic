683eadfc24cbdc7b9db8847501bd7a45702e0552
sv <svyatozar@gmail.com>
2025-02-12 09:25:47 -0500

    on button click change its label

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace tic
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "X";
+        }
     }
 }
