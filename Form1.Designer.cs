f119f7b700ab23958858fdbb6bb4e0fcaf5bf73f
sv <svyatozar@gmail.com>
2025-02-18 00:56:53 -0500

    added 'running' game state variable and restart mechanism using New Game button. The status bar is now updated from single function update_status_bar()

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,6 +38,7 @@
             cell6 = new Cell();
             cell7 = new Cell();
             cell8 = new Cell();
+            new_game_btn = new Button();
             SuspendLayout();
             // 
             // label1
@@ -45,14 +46,16 @@
             label1.AutoSize = true;
             label1.Location = new Point(18, 10);
             label1.Name = "label1";
-            label1.Size = new Size(47, 15);
+            label1.Size = new Size(50, 15);
             label1.TabIndex = 1;
             label1.Text = "X's turn:";
             // 
             // cell0
             // 
+            cell0.Font = new Font("Segoe UI", 70F);
+            cell0.ForeColor = Color.Blue;
             cell0.Location = new Point(18, 44);
-            cell0.Name = "0";
+            cell0.Name = "cell0";
             cell0.Size = new Size(132, 136);
             cell0.TabIndex = 3;
             cell0.UseVisualStyleBackColor = true;
@@ -60,73 +63,100 @@
             // 
             // cell1
             // 
+            cell1.Font = new Font("Segoe UI", 70F);
+            cell1.ForeColor = Color.Blue;
             cell1.Location = new Point(156, 44);
-            cell1.Name = "1";
+            cell1.Name = "cell1";
             cell1.Size = new Size(119, 136);
             cell1.TabIndex = 4;
             cell1.Click += cell_Click;
             // 
             // cell2
             // 
+            cell2.Font = new Font("Segoe UI", 70F);
+            cell2.ForeColor = Color.Blue;
             cell2.Location = new Point(281, 44);
-            cell2.Name = "2";
+            cell2.Name = "cell2";
             cell2.Size = new Size(124, 136);
             cell2.TabIndex = 5;
             cell2.Click += cell_Click;
             // 
             // cell3
             // 
+            cell3.Font = new Font("Segoe UI", 70F);
+            cell3.ForeColor = Color.Blue;
             cell3.Location = new Point(18, 186);
-            cell3.Name = "3";
+            cell3.Name = "cell3";
             cell3.Size = new Size(132, 139);
             cell3.TabIndex = 6;
             cell3.Click += cell_Click;
             // 
             // cell4
             // 
+            cell4.Font = new Font("Segoe UI", 70F);
+            cell4.ForeColor = Color.Blue;
             cell4.Location = new Point(156, 186);
-            cell4.Name = "4";
+            cell4.Name = "cell4";
             cell4.Size = new Size(119, 139);
             cell4.TabIndex = 7;
             cell4.Click += cell_Click;
             // 
             // cell5
             // 
+            cell5.Font = new Font("Segoe UI", 70F);
+            cell5.ForeColor = Color.Blue;
             cell5.Location = new Point(281, 186);
-            cell5.Name = "5";
+            cell5.Name = "cell5";
             cell5.Size = new Size(124, 139);
             cell5.TabIndex = 8;
             cell5.Click += cell_Click;
             // 
             // cell6
             // 
+            cell6.Font = new Font("Segoe UI", 70F);
+            cell6.ForeColor = Color.Blue;
             cell6.Location = new Point(18, 331);
-            cell6.Name = "6";
+            cell6.Name = "cell6";
             cell6.Size = new Size(132, 133);
             cell6.TabIndex = 9;
             cell6.Click += cell_Click;
             // 
             // cell7
-            //
+            // 
+            cell7.Font = new Font("Segoe UI", 70F);
+            cell7.ForeColor = Color.Blue;
             cell7.Location = new Point(156, 331);
-            cell7.Name = "7";
+            cell7.Name = "cell7";
             cell7.Size = new Size(119, 133);
             cell7.TabIndex = 10;
             cell7.Click += cell_Click;
             // 
             // cell8
-            //
+            // 
+            cell8.Font = new Font("Segoe UI", 70F);
+            cell8.ForeColor = Color.Blue;
             cell8.Location = new Point(281, 331);
-            cell8.Name = "8";
+            cell8.Name = "cell8";
             cell8.Size = new Size(124, 133);
             cell8.TabIndex = 11;
             cell8.Click += cell_Click;
+            // 
+            // new_game_btn
+            // 
+            new_game_btn.Location = new Point(330, 12);
+            new_game_btn.Name = "new_game_btn";
+            new_game_btn.Size = new Size(75, 23);
+            new_game_btn.TabIndex = 12;
+            new_game_btn.Text = "New Game";
+            new_game_btn.UseVisualStyleBackColor = true;
+            new_game_btn.Click += new_game_btn_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(427, 492);
+            Controls.Add(new_game_btn);
             Controls.Add(label1);
             Controls.Add(cell0);
             Controls.Add(cell1);
@@ -149,5 +179,6 @@
         private Cell cell0, cell1, cell2,
             cell3, cell4, cell5,
             cell6, cell7, cell8;
+        private Button new_game_btn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ namespace tic
 {
     public partial class Form1 : Form
     {
+        private bool running = true; // is game running?
         private int curo = 0; // 1 when it is O's move?
         private int board = 0; // board state
         // board state is a packed binary
@@ -46,13 +47,14 @@ namespace tic
             if (c != 0 &&
                 ((cval(b, 1) == c && cval(b, 2) == c) ||
                  (cval(b, 3) == c && cval(b, 6) == c))
-               ) {
+               )
+            {
                 return c;
             }
 
             // check right and bottom sides:
             c = cval(b, 8);
-            if(c != 0 &&
+            if (c != 0 &&
                 ((cval(b, 2) == c && cval(b, 5) == c) ||
                  (cval(b, 6) == c && cval(b, 7) == c))
                  )
@@ -61,55 +63,93 @@ namespace tic
             }
 
             // if there is no empty cells, it's a draw - return 3
-            if(cval(b, 0) != 0 && cval(b, 1) != 0 && cval(b, 2) != 0 &&
+            if (cval(b, 0) != 0 && cval(b, 1) != 0 && cval(b, 2) != 0 &&
                cval(b, 3) != 0 && cval(b, 4) != 0 && cval(b, 5) != 0 &&
-               cval(b, 6) != 0 && cval(b, 7) != 0 && cval(b, 8) != 0) {
+               cval(b, 6) != 0 && cval(b, 7) != 0 && cval(b, 8) != 0)
+            {
                 return 3;
             }
 
             return 0;
         }
+
+        private void update_status_bar()
+        {
+            if(!running)
+            {
+                int win = eval_win(board);
+                switch (win)
+                {
+                    case 1:
+                        // X won the game
+                        label1.Text = "Game over. X won!";
+                        break;
+                    case 2:
+                        // O won the game
+                        label1.Text = "Game over. O won!";
+                        break;
+                    case 3:
+                        // draw
+                        label1.Text = "Game over. It's a draw!";
+                        break;
+                }
+            } else
+            {
+                label1.Text = curo == 1 ? "O's Turn:" : "X's Turn:";
+            }
+        }
+
         private void cell_Click(object sender, EventArgs e)
         {
-            Cell senderCell = (Cell)sender;
-            if(senderCell.Text != "")
+            if (!running)
             {
                 return;
             }
-            int x = Int32.Parse(senderCell.Name);
+            Cell senderCell = (Cell)sender;
+            if (senderCell.Text != "")
+            {
+                return;
+            }
+            int x = Int32.Parse(senderCell.Name.Substring(4));
 
             if (curo == 1)
             {
                 // O's Turn: put O
                 senderCell.Text = "O";
                 curo = 0;
-                label1.Text = "X's turn:";
                 board |= 2 << (x * 2);
-            } else
+            }
+            else
             {
                 // X's Turn: put X
                 senderCell.Text = "X";
                 curo = 1;
-                label1.Text = "O's Turn:";
                 board |= 1 << (x * 2);
             }
 
             int win = eval_win(board);
-            switch (win)
+            if (win != 0)
             {
-                case 1:
-                    // X won the game
-                    label1.Text = "Game over. X won!";
-                    break;
-                case 2:
-                    // O won the game
-                    label1.Text = "Game over. O won!";
-                    break;
-                case 3:
-                    // draw
-                    label1.Text = "Game over. It's a draw!";
-                    break;
+                running = false;
             }
+            update_status_bar();
+        }
+
+        private void new_game_btn_Click(object sender, EventArgs e)
+        {
+            board = 0;
+            cell0.Text = "";
+            cell1.Text = "";
+            cell2.Text = "";
+            cell3.Text = "";
+            cell4.Text = "";
+            cell5.Text = "";
+            cell6.Text = "";
+            cell7.Text = "";
+            cell8.Text = "";
+            curo = 0;
+            running = true;
+            update_status_bar();
         }
     }
 }
